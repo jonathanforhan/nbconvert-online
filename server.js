f2ff33651d5d5a96601f70187ee7d89aa57c0c82
jonathanforhan <jonathan.forhan@gmail.com>
2024-03-02 15:36:19 -0500

    polish

--- server.js
+++ server.js
@@ -13,12 +13,14 @@ app.use(fileUpload());
 app.set("view engine", "ejs");
 
 app.get("/", (_req, res) => {
-  res.render("index", { text: "World" });
+  res.render("index");
 });
 
 app.post("/upload", (req, res) => {
   if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send("no files uploaded, try again.");
+    return res
+      .status(400)
+      .render("index", { content: "no files uploaded, try again." });
   }
 
   const ipynb = req.files.ipynb;
@@ -29,12 +31,15 @@ app.post("/upload", (req, res) => {
   try {
     fs.mkdirSync(uidPath);
   } catch (e) {
-    return res.status(500).send(JSON.stringify(e));
+    return res.status(500).render("index", {
+      content: "server mkdir error",
+      errorMessage: JSON.stringify(e),
+    });
   }
 
   ipynb.mv(path.join(uidPath, ipynb.name), (err) => {
     if (err) {
-      return res.status(500).send(err);
+      return res.status(500).render("index", { content: "server mv error" });
     }
 
     const ipynbToPdf = spawn("jupyter", [
@@ -45,22 +50,22 @@ app.post("/upload", (req, res) => {
       `--output-dir=${uidPath}`,
     ]);
 
-    ipynbToPdf.stdout.on("data", (data) => console.log(`stdout: ${data}`));
-    ipynbToPdf.stderr.on("data", (data) => console.log(`stderr: ${data}`));
-    ipynbToPdf.on("close", (code) => {
-      console.log(`child process exited with code ${code}`);
-      if (code === 0) {
-        res.status(301).redirect(`/download/${uid}/${fileName}`);
-      } else {
-        res.status(400).send(`child process exited with code ${code}`);
-      }
-    });
+    errorLog = [];
+    ipynbToPdf.stderr.on("data", (data) => errorLog.push(data));
+    ipynbToPdf.on("close", (code) =>
+      code === 0
+        ? res.status(301).redirect(`/download/${uid}/${fileName}`)
+        : res.status(400).render("index", {
+            content: `child process exited with code ${code}`,
+            errorMessage: errorLog.slice(1).join(""),
+          }),
+    );
   });
 });
 
 app.get("/download/:uid/:fileName", (req, res) => {
   if (!req.params.uid || !req.params.fileName) {
-    return res.status(500).send("invalid path params");
+    return res.status(500).render("index", { content: "invalid path params" });
   }
 
   (function ({ uid, fileName }) {
